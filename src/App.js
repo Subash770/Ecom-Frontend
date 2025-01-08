fe07cadd340bd02d043b6e4a4dd12ef3d6ef44c0
Subash <spidybeatz25@gmail.com>
2025-01-08 23:15:04 +0530

    navbar and bottom row design

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Cart from './pages/Cart';
-import './App.css';
-
-
-import WelcomeSection from './Components/WelcomeSection';
-import ShopSection from './Components/ShopSection';
-import BenefitsSection from './Components/BenefitsSection';
-import OffersSection from './Components/OffersSection';
-
+import ProductRow from './Components/ProductRow';  // Correct path to your ProductRow component
+import OfferSlider from './Components/OfferSlider';
+import ElectronicsCarousel from './Components/ElectronicsCarousel';
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
-    
-    </Router>
-    
+    <div>
+      <Navbar />
+      <ProductRow />
+      <OfferSlider />
+      <ElectronicsCarousel/>
+    </div>
   );
 }
+
 
 export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState } from "react";
+import "./Navbar.css";
 
-function Navbar() {
+const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isLoginOpen, setLoginOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">EcomShop</div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
-      </ul>
+      <div className="navbar-left">
+        <div className="logo">YourLogo</div>
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search for products, brands, and more"
+        />
+      </div>
+
+      <div className="navbar-right">
+        <div
+          className="dropdown"
+          onMouseEnter={() => setLoginOpen(true)}
+          onMouseLeave={() => setLoginOpen(false)}
+        >
+          <button className="login-btn">Login</button>
+          {isLoginOpen && (
+            <div className="dropdown-content">
+              <ul>
+                <li className="dropdown-item">New Customer Sign Up</li>
+                <li className="dropdown-item">My Profile</li>
+                <li className="dropdown-item">Orders</li>
+                <li className="dropdown-item">Wishlist</li>
+                <li className="dropdown-item">Rewards</li>
+                <li className="dropdown-item">Gift Card</li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div className="cart">Cart</div>
+        <div className="seller">Become a Seller</div>
+
+        <div
+          className="dropdown"
+          onMouseEnter={() => setMenuOpen(true)}
+          onMouseLeave={() => setMenuOpen(false)}
+        >
+          <button className="menu-btn">⋮</button>
+          {isMenuOpen && (
+            <div className="dropdown-content">
+              <ul>
+                <li className="dropdown-item">Notification Preferences</li>
+                <li className="dropdown-item">24x7 Customer Care</li>
+                <li className="dropdown-item">Advertise</li>
+                <li className="dropdown-item">Download App</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
